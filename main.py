a5b274a08e43375df9defa7f98335c3b894114b1
Kerimov Bulat <Bulatker@gmail.com>
2019-02-12 09:59:26 +0100

    added 2D animation

--- main.py
+++ main.py
@@ -3,24 +3,23 @@ import numpy as np
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from matplotlib.animation import FuncAnimation
-import time
 from mpl_toolkits.mplot3d import Axes3D
+
 
 # Global parameters
 A = 0.7
-B = 2
+B = 1.9
 C = 2
 Vmax = 10
 num_of_particles = 20
-num_of_iterations = 100
+num_of_iterations = 1000
 x_lim = 10
 y_lim = 10
-benchmark_function = "rosenbrock"  # rosenbrock/rastrigin
+benchmark_function = "rastrigin"  # rosenbrock/rastrigin
 
 # Initialzing parameters
-X = np.random.uniform(-2*x_lim, 2*x_lim, size = num_of_particles)
-Y = np.random.uniform(-2*y_lim, 2*y_lim, size = num_of_particles)
+X = np.random.uniform(-2 * x_lim, 2 * x_lim, size = num_of_particles)
+Y = np.random.uniform(-2 * y_lim, 2 * y_lim, size = num_of_particles)
 Vx = np.random.rand(num_of_particles)
 Vy = np.random.rand(num_of_particles)
 Xpb = X  # particle best value's X
@@ -31,7 +30,6 @@ X_hist = np.zeros((num_of_particles, num_of_iterations))
 Y_hist = np.zeros((num_of_particles, num_of_iterations))
 Vx_hist = np.zeros((num_of_particles, num_of_iterations))
 Vy_hist = np.zeros((num_of_particles, num_of_iterations))
-
 
 
 def get_value(X, Y, benchmark_function):
@@ -47,13 +45,17 @@ def get_value(X, Y, benchmark_function):
         return Z
 
 
+"""
+Main algorithm starts here
+"""
+
+# Calculate initial best values basing on random initial parameters
 for idx in range(num_of_particles):
     Fpb[idx] = get_value(X[idx], Y[idx], benchmark_function)
 Fgb = np.amin(Fpb)
 Xgb = X[np.argmin(Fpb)]
 Ygb = Y[np.argmin(Fpb)]
 
-# Main algorithm
 for epoch in range(num_of_iterations):
     if epoch % 5 == 0:
         print(
@@ -61,7 +63,7 @@ for epoch in range(num_of_iterations):
             Xgb + ", %.3f" % Ygb + ")")
 
     for idx in range(num_of_particles):
-    # update speeds
+        # update speeds
         Rgx = np.random.uniform()
         Rgy = np.random.uniform()
         Rpx = np.random.uniform()
@@ -100,8 +102,10 @@ for epoch in range(num_of_iterations):
             Xgb = X[idx]
             Ygb = Y[idx]
 
-# Plotting the results
 
+"""
+Plotting methods
+"""
 def plot_results_animate_3d(benchmark_function, fig = plt.figure(), plot_function = True):
     """
     Plotting the animation of the results in 3D
@@ -141,11 +145,9 @@ def plot_results_animate_3d(benchmark_function, fig = plt.figure(), plot_functio
                                              frames = num_of_iterations, interval = 100, blit = False)
     plt.show()
 
-
-
 def plot_results_2d(benchmark_function, fig = plt.figure(), plot_function = True):
     """
-    Plotting the animation of the results in 2D with Heatmap
+    Plotting the static results in 2D with Heatmap
     """
     fig, ax = plt.subplots(1, 1)
 
@@ -174,6 +176,48 @@ def plot_results_2d(benchmark_function, fig = plt.figure(), plot_function = True
     ax.scatter(X_hist[:, num_of_iterations - 1], Y_hist[:, num_of_iterations - 1], c = "y")
     plt.show()
 
+def plot_results_animate_2d(benchmark_function, fig = plt.figure(), plot_function = True):
+    """
+    Plotting the animation of the results in 2D with Heatmap
+    """
+    ax = plt.axes(xlim = (-x_lim, x_lim), ylim = (-y_lim, y_lim))
 
-# plot_results_animate_3d(benchmark_function, plot_function = False)
-plot_results_2d(benchmark_function)
+    if plot_function:
+        if benchmark_function == "rastrigin":
+            X = np.linspace(-x_lim, x_lim, 50)
+            Y = np.linspace(-y_lim, y_lim, 50)
+            X, Y = np.meshgrid(X, Y)
+            Z = (X ** 2 - 10 * np.cos(2 * np.pi * X)) + (Y ** 2 - 10 * np.cos(2 * np.pi * Y)) + 20
+            ax.set_xlim(-x_lim, x_lim)
+            ax.set_ylim(-y_lim, y_lim)
+            ax.pcolor(X, Y, Z)
+
+        elif benchmark_function == "rosenbrock":
+            X = np.linspace(-x_lim, x_lim, 50)
+            Y = np.linspace(-y_lim, y_lim, 50)
+            X, Y = np.meshgrid(X, Y)
+            Z = (1 - X) ** 2 + 100 * (Y - X ** 2) ** 2
+            ax.set_xlim(-x_lim, x_lim)
+            ax.set_ylim(-y_lim, y_lim)
+            ax.pcolor(X, Y, Z, cmap = cm.nipy_spectral)
+
+    c = ax.imshow(Z, cmap = cm.nipy_spectral)
+
+    fig.colorbar(c, ax = ax)
+    sc = ax.scatter(X_hist[:, num_of_iterations - 1], Y_hist[:, num_of_iterations - 1], c = "y")
+
+    def animate(i):
+        data = [list(X_hist[:, i]), list(Y_hist[:, i])]
+        data = list(map(list, zip(*data)))
+
+        sc.set_offsets(data)
+        return sc,
+
+    ani = matplotlib.animation.FuncAnimation(fig, animate,
+                                             frames = num_of_iterations, interval = 100, blit = False)
+    plt.show()
+
+
+
+plot_results_animate_3d(benchmark_function, plot_function = False)
+plot_results_animate_2d(benchmark_function)
